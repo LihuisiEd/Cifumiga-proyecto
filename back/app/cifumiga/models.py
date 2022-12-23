c169afa7cd794b6d5f56f93341ee4938976f433e
LihuisiEd <eduardo.lihuisi@tecsup.edu.pe>
2022-12-22 21:00:07 -0500

    API REST

--- back/app/cifumiga/models.py
+++ back/app/cifumiga/models.py
@@ -1,61 +1,137 @@
+from email.policy import default
+from enum import unique
+from unittest.util import _MAX_LENGTH
+from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models her
-
 class Cliente(models.Model):
-   id_cliente = models.CharField(max_length=200, null=True, blank=True)
-   nombre = models.TextField(null=True, blank=True)
-   ruc = models.CharField(max_length=200, null=True, blank=True)
-   id_certificado = models.CharField(max_length=200, null=True, blank=True)
-   fecha_creacion = models.DateTimeField(auto_now=True)
+    cliente_nombre = models.CharField(max_length=255)
+    cliente_ruc = models.CharField(max_length=200, null=True, blank=True)
+    cli_fecregistro = models.DateTimeField(auto_now=True)
+    cli_fecmodificacion = models.DateTimeField(auto_now=True)
 
-   def __str__(self):
-      return self.id_cliente
+    def __str__(self):
+        return self.cli_nombre
 
-class Empleado(models.Model):
-   usu_id = models.CharField(max_length=200, null=True, blank=True)
-   nombre_emp = models.TextField(null=True, blank=True)
-   email_emp = models.TextField(null=True, blank=True)
-   pass_emp = models.TextField(null=True, blank=True)
-   num_emp = models.TextField(null=True, blank=True)
-   id_certificado = models.CharField(max_length=200, null=True, blank=True)
-   emp_feccreacion = models.DateTimeField(auto_now=True)
-   emp_modificacion = models.DateTimeField(auto_now=True)
+class Empleado(AbstractUser):
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    celular = models.CharField(max_length=9)
+    emp_feccreacion = models.DateTimeField(auto_now_add=True)
+    emp_modificacion = models.DateTimeField(auto_now=True)
+
+    username = None
+    is_staff = True
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+class Certificado(models.Model):
+    cliente = models.ForeignKey(Cliente, related_name='certificado_cliente', on_delete=models.CASCADE)
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField()
+    doc_certificado = models.FileField(upload_to="certificados/", null=True, blank=True)
+    cert_feccreacion = models.DateTimeField(auto_now=True)
+
 
 class Operacion(models.Model):
-   id_operacion = models.CharField(max_length=200, null=True, blank=True)
-   contacto = models.TextField(null=True, blank=True)
-   correo = models.CharField(max_length=200, null=True, blank=True)
-   id_cliente = models.CharField(max_length=200, null=True, blank=True)
+    cliente = models.ForeignKey(Cliente, related_name='operacion_cliente', on_delete=models.CASCADE)
+    operacion_contacto = models.CharField(max_length=50)
+    operacion_correo = models.CharField(max_length=200)
+
+
 
 class Factura(models.Model):
-   id_operacion = models.CharField(max_length=200, null=True, blank=True)
-   contacto = models.TextField(null=True, blank=True)
-   correo = models.CharField(max_length=200, null=True, blank=True)
-   servicio_costo = models.CharField(max_length=200, null=True, blank=True)
-   id_cliente = models.CharField(max_length=200, null=True, blank=True)
-   doc_factura = models.TextField(null=True, blank=True)
+    cliente = models.ForeignKey(Cliente, related_name='factura_cliente', on_delete=models.CASCADE)
+    factura_contacto = models.CharField(max_length=50)
+    factura_correo = models.CharField(max_length=200, null=True, blank=True)
+    factura_costo = models.CharField(max_length=20)
+    doc_factura = models.FileField(upload_to="facturas/", null=True, blank=True)
 
-class documentoxcliente(models.Model):
-    dxa_id = models.CharField(max_length=200, null=True, blank=True)
-    descripcion = models.TextField()
-    id_cliente = models.CharField(max_length=200, null=True, blank=True)
+    def __str__(self):
+        return self.cliente.cli_nombre
 
-class documento_doc(models.Model):
-    doc_id = models.CharField(max_length=200, null=True, blank=True)
-    doc_tipo = models.TextField(null=True, blank=True)
-    doc_fecdoc= models.TextField(null=True,blank=True)
-    doc_archivo= models.TextField(null=True,blank=True)
+class Tipo_servicio(models.Model):
+    servicio_nombre = models.CharField(max_length=100)
 
-class informe(models.Model):
-   id_informe = models.CharField(max_length=200, null=True, blank=True)
-   fecha_entrega= models.DateTimeField(auto_now=True)
-   id_cliente= models.CharField(max_length=200, null=True, blank=True)
-   res_prom = models.TextField()
-   id_tipo_fum= models.CharField(max_length=200, null=True, blank=True)
-   problema_desc = models.TextField(null=True, blank=True)
-   area_problema = models.CharField(max_length=200, null=True, blank=True)
-   id_respuesta = models.CharField(max_length=200, null=True, blank=True)
+class Servicio(models.Model):
+    tipo = models.ForeignKey(Tipo_servicio, related_name='tipo_servicio', on_delete=models.CASCADE)
+    cliente = models.ForeignKey(Cliente, related_name='cliente', on_delete=models.CASCADE)
+    servicio_desc = models.TextField(max_length=150)
+    servicio_area = models.CharField(max_length=50)
+    servicio_dim = models.CharField(max_length=20)
+    servicio_frec = models.CharField(max_length=100)
+    servicio_precio = models.CharField(max_length=20)
+    doc_contrato = models.FileField(upload_to="contratos/", null=True, blank=True)
+    doc_proforma = models.FileField(upload_to="proformas/", null=True, blank=True)
+    doc_ficha = models.FileField(upload_to="fichas/", null=True, blank=True)
 
-class tipo_fumigacion(models.Model):
-    id_tipo_fumigacion = models.CharField(max_length=200, null=True, blank=True)
+    def __str__(self):
+        return self.tipo.tserv_nombre
+
+
+class Tipo_Documento(models.Model):
+    documento_nombre = models.CharField(max_length=100)
+
+
+class Documento(models.Model):
+    documento_tipo = models.ForeignKey(Tipo_Documento, related_name='tipo_documento', on_delete=models.CASCADE)
+    doc_archivo = models.FileField(upload_to="archivos/", null=True, blank=True)
+    documento_fecha = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.documento_tipo.tipo_documento
+
+class Documento_Cliente(models.Model):
+    cliente = models.ForeignKey(Cliente, related_name='documento_cliente', on_delete=models.CASCADE)
+    documento = models.ForeignKey(Documento, related_name='documento', on_delete=models.CASCADE)
+
+
+class Nivel_tservicio(models.Model):
+    tipo = models.ForeignKey(Tipo_servicio, related_name='tipo', on_delete=models.CASCADE)
+    nserv_nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nserv_nombre
+
+class Informe(models.Model):
+    informe_mes = models.CharField(max_length=20)
+    informe_sostenimiento = models.TextField(max_length=150)
+    doc_informe = models.FileField(upload_to="informes/", null=True, blank=True)
+    
+    def __str__(self):
+        return self.info_mes
+
+
+class Seguridad_Cliente(models.Model):
+    cliente = models.ForeignKey(Cliente, related_name='seguridad_cliente', on_delete=models.CASCADE)
+    doc_iperc = models.FileField(upload_to="ipercs/", null=True, blank=True)
+    doc_ats = models.FileField(upload_to="atss/", null=True, blank=True)
+    
+    def __str__(self):
+        return self.cliente.cli_nombre
+
+class Seguridad(models.Model):
+    doc_productos = models.FileField(upload_to="productos/", null=True, blank=True)
+    doc_pets = models.FileField(upload_to="pets/", null=True, blank=True)
+
+class Tramites(models.Model):
+    tipo = models.ForeignKey(Tipo_servicio, related_name='tipo_tramite', on_delete=models.CASCADE)
+    cliente = models.ForeignKey(Cliente, related_name='tramite_cliente', on_delete=models.CASCADE)
+    tramite_fecha = models.DateField()
+    tramite_contacto = models.CharField(max_length=50)
+    tramite_tipo = models.CharField(max_length=100)
+    tramite_descripcion = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.cliente.cli_nombre
+    
+class Comentario(models.Model):
+    cliente = models.ForeignKey(Cliente, related_name='comentario_cliente', on_delete=models.CASCADE)
+    comentario = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.cliente.cli_nombre
--- back/app/cifumiga_api/views.py
+++ back/app/cifumiga_api/views.py
@@ -1,3 +1,303 @@
+from django.http import Http404
 from django.shortcuts import render
 
+from cifumiga.models import *
+
+#Imports relacionado a rest framework
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from cifumiga_api.serializers import *
+from rest_framework import status
+from rest_framework.exceptions import AuthenticationFailed
+
+import jwt, datetime
+
+import cloudinary.uploader
+
+import requests
+
 # Create your views here.
+class Index(APIView):
+    def get(self, request):
+        context = {
+            'status':True,
+            'content':'api activa'
+        }
+        return Response(context)
+
+class ClienteAPIGeneral(APIView):
+    def get(self, request):
+        cliLista = Cliente.objects.all()
+        serializer = ClienteSerializer(cliLista, many=True)
+
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ClienteSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ClienteAPIDetalle(APIView):
+    def get_object(self, empleado_id):
+        try:
+            return Cliente.objects.get(pk=empleado_id)
+        except Cliente.DoesNotExist:
+            raise Http404
+
+    def get(self, request, empleado_id):
+        empListaDet = self.get_object(empleado_id)
+        empSerializer = ClienteSerializer(empListaDet)
+        
+        return Response(empSerializer.data)
+
+    def put(self, request, empleado_id):
+        empListaDet = self.get_object(empleado_id)
+        empSerializer = ClienteSerializer(empListaDet, data=request.data)
+
+        if empSerializer.is_valid():
+            empSerializer.save()
+            return Response(empSerializer.data)
+        
+        return Response(empSerializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, empleado_id):
+        empListaDet = self.get_object(empleado_id)
+        empListaDet.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+class RegisterView(APIView):
+    def post(self, request):
+        serializers = EmpleadoSerializer(data=request.data)
+        serializers.is_valid(raise_exception=True)
+        serializers.save()
+        return response(serializers.data)
+
+class LoginView(APIView):
+    def post (self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        emp = Empleado.objects.filter(email=email).first()
+
+        if emp is None:
+            raise AuthenticationFailed('Empleado no encontrado!')
+
+        if not emp.check_password(password):
+            raise AuthenticationFailed('Contraseña incorrecta!')
+
+        payload = {
+            'id': emp.id,
+            'name':emp.name,
+            'email':emp.email,
+            'celular':emp.celular,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            'iat': datetime.datetime.utcnow()
+        }
+
+        token = jwt.encode(payload, 'secret', algorithm='HS256').decode('utf-8')
+
+        response = Response()
+
+        response.set_cookie(key='jwt', value=token, httponly=True)
+        response.data = {
+            'jwt': token
+        }
+        return response
+
+
+class EmpleadoAPIGeneral(APIView):
+
+
+    def get(self, request):
+        token = request.COOKIES.get('jwt')
+
+        if not token:
+            raise AuthenticationFailed('Usuario no autenticado')
+        try:
+            payload = jwt.decode(token, 'secret', algorithm=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Usuario no autenticado')
+
+        emp = Empleado.objects.filter(id=payload['id']).first()
+        serializer =  EmpleadoSerializer(emp)
+
+        return Response(serializer.data)
+
+    def put(self, request):
+
+        token = request.COOKIES.get('jwt')
+
+        if not token:
+            raise AuthenticationFailed('Usuario no autenticado')
+        try:
+            payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Usuario no autenticado')
+        emp = Empleado.objects.filter(id=payload['id']).first()
+
+        serializer = EmpleadoSerializer(emp, data=request.data)
+
+        context = {
+            'status':True,
+            'content':serializer.data   
+        }
+
+        return Response(context) 
+
+    def delete(self, request):
+        token = request.COOKIES.get('jwt')
+
+        if not token:
+            raise AuthenticationFailed('Usuario no autenticado')
+        try:
+            payload = jwt.decode(token, 'secret', algorithm=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Usuario no autenticado')
+        emp = Empleado.objects.filter(id=payload['id']).first()
+
+        emp.delete()
+
+        context = {
+            'status': True,
+            'message':'Usuario Eliminado'
+        }
+
+        return Response(context)
+
+class LogoutView(APIView):
+    def post(self, request):
+        response = Response()
+        response.delete_cookie('jwt')
+        response.data = {
+            "message":"Logout successful"
+        }
+        return response
+
+class CertificadoAPI(APIView):
+    def get(self, request):
+        certificado = Certificado.objects.all()
+        serializer = CertificadoSerializer(certificado, many=True)
+
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = CertificadoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CertificadoAPIDetalle(APIView):
+    def get_object(self, clienteid):
+        try:
+            return Certificado.objects.get(pk=clienteid)
+        except Certificado.DoesNotExist:
+            raise Http404
+
+    def get(self, request, clienteid):
+        certificado = Certificado.objects.filter(cliente=clienteid)
+        serializer = CertificadoSerializer(certificado, many=True)
+
+        return Response(serializer.data)
+    
+    def put(self, request, clienteid):
+        certificado = self.get_object(clienteid)
+        serializer = CertificadoSerializer(certificado, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+    def delete(self, request, clienteid, format=None):
+        certificado = Certificado.objects.get(pk=clienteid)
+        certificado.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class FacturaAPI(APIView):
+    def get(self, request):
+        factura = Factura.objects.all()
+        serializer = FacturaSerializer(factura, many=True)
+
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = FacturaSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ServicioAPI(APIView):
+    def get(self, request):
+        servicio = Servicio.objects.all()
+        serializer = ServicioSerializer(servicio, many=True)
+
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = ServicioSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ServicioAPIDetalle(APIView):
+    def get_object(self, clienteid):
+        try:
+            return Servicio.objects.get(pk=clienteid)
+        except Servicio.DoesNotExist:
+            raise Http404
+
+    def get(self, request, clienteid):
+        servicio = Servicio.objects.filter(cliente=clienteid)
+        serializer = ServicioSerializer(servicio, many=True)
+
+        return Response(serializer.data)
+    
+
+    def delete(self, request, clienteid, format=None):
+        servicio = Servicio.objects.get(pk=clienteid)
+        servicio.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class TipoServicioAPI(APIView):
+    def get(self, request):
+        tiposervicio = Tipo_servicio.objects.all()
+        serializer = TipoServicioSerializer(tiposervicio, many=True)
+
+        return Response(serializer.data)
+
+class NivelServicioAPI(APIView):
+    def get(self, request):
+        nivelservicio = Nivel_tservicio.objects.all()
+        serializer = NivelServicioSerializer(nivelservicio, many=True)
+
+        return Response(serializer.data)
+
+class NivelServicioAPIDetalle(APIView):
+    def get_object(self, tipo_id):
+        try:
+            return Nivel_tservicio.objects.get(pk=tipo_id)
+        except Nivel_tservicio.DoesNotExist:
+            raise Http404
+
+    def get(self, request, tipo_id):
+        nivel = Nivel_tservicio.objects.filter(tipo=tipo_id)
+        serializer = NivelServicioSerializer(nivel, many=True)
+
+        return Response(serializer.data)
+    
+
+    def delete(self, request, tipo_id, format=None):
+        nivel = Nivel_tservicio.objects.get(pk=tipo_id)
+        nivel.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
